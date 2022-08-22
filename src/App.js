41d607d37df31bf5b58ba28a7017df49d0faabc4
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-08-22 20:44:54 +0700

    Google auth login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from "./logo.svg"
 import "./App.css"
+import { GoogleLogin, GoogleLogout } from "react-google-login"
+import { gapi } from "gapi-script"
+import { useState, useEffect } from "react"
 
 function App() {
+  const clientId =
+    "434016079883-16upiqfv4b4gamkgdog1mvg4jeh0bt0b.apps.googleusercontent.com"
+
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.clientId.init({
+        clientId: clientId,
+        scope: "",
+      })
+    }
+    gapi.load("client:auth2", initClient)
+  }, [])
+
+  const onSuccess = (res) => {
+    setProfile(res.profileObj)
+    console.log("success", res)
+  }
+
+  const onFailure = (res) => {
+    console.log("failed", res)
+  }
+
+  const logOut = () => {
+    setProfile(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Google login</h2>
+      <br></br>
+      {profile ? (
+        <div>
+          <img src={profile.imageUrl} alt="" />
+          <p>Name: {profile.name}</p>
+          <p>Email: {profile.email}</p>
+          <GoogleLogout
+            clientId={clientId}
+            buttonText="Log out"
+            onLogoutSuccess={logOut}
+          />
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign in with Google"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      )}
     </div>
   )
 }
