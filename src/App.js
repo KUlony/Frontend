98b2c9d2648b5462bf0b8a5abd14f201cd4be2e5
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-08-27 22:26:22 +0700

    add react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg"
 import "./App.css"
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar.js"
 
 function App() {
   return (
-    <div><NavBar /></div>
+    <div>
+      <NavBar />
+    </div>
   )
 }
 
