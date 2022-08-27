7fe6df28f0849c8d4630f8aaeacf36ea1984e061
maikan3002 <kankasem3002@gmail.com>
2022-08-28 00:08:37 +0700

    Home

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import "./Home.css";
+import Think from "./think.png";
+import search from "./search.png";
+
 function Home() {
   return (
     <div>
@@ -10,8 +13,18 @@ function Home() {
       <div className="home_search">
         <div className="home_search_lorem">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          samjdnmlaskmdlksadl kaslk dmaslkdmlkasm
         </div>
-        <div></div>
+        <img src={Think} alt="Girl in a jacket" className="think_img"></img>
+        <div className="home_search_input">
+          <form>
+            {/* <label>maikan</label> */}
+            <input type="text" required className="search_input" />
+            <button className="search-button">
+              <img src={search} width="10px" height="10px" />
+            </button>
+          </form>
+        </div>
       </div>
       <div className="Home_post">koksok</div>
     </div>
