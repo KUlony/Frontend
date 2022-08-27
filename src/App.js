57d3df158d6cc1ab891e52b81786b51c358d8202
maikan3002 <kankasem3002@gmail.com>
2022-08-27 22:23:59 +0700

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg"
-import "./App.css"
-import NavBar from "./components/NavBar"
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./Home";
 
 function App() {
   return (
-    <div><NavBar /></div>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Home;
