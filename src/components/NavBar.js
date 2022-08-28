3073894d8b47c79a3716c83818b14efd13d41cdb
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-08-28 22:19:49 +0700

    done component not include Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import './NavBar.css'
-import {FaRegBell} from 'react-icons/fa'
+import React from "react"
+import "./NavBar.css"
+import { FaRegBell } from "react-icons/fa"
 
 function NavBar() {
   return (
-    <div className='Container'>
-        <div className='Kulony'>KULONY</div>
-        <div className='Home'>HOME</div>
-        <div className='MyPost'>MY POST</div>
-        <div className='RequestTopic'>REQUEST TOPIC</div>
-        <div className='CreatePost'>Create new post</div>
-        <FaRegBell className='Bell' size={20}/>
-        
+    <div className="Container">
+      <div className="Kulony">KULONY</div>
+      <div className="Home">HOME</div>
+      <div className="MyPost">MY POST</div>
+      <div className="RequestTopic">REQUEST TOPIC</div>
+      <div className="CreatePost">Create new post</div>
+      <FaRegBell className="Bell" size={20} />
     </div>
   )
 }
--- src/pages/Createpost.js
+++ src/pages/Createpost.js
@@ -1,41 +1,91 @@
 import React from "react"
-import NavBar from "../components/NavBar"
+// import NavBar from "../components/NavBar"
 import "./Createpost.css"
 
 function Createpost() {
+  // let textArea = document.getElementById("intputT")
+  // let characterCounter = document.getElementById("char_count")
+  // const maxNumOfChars = 70
+
+  // const countCharacters = () => {
+  //   let numOfEnteredChars = textArea.value.length
+  //   let counter = maxNumOfChars - numOfEnteredChars
+  //   characterCounter.textContent = counter + "/70"
+
+  //   if (counter <= 5) {
+  //     characterCounter.style.color = "red"
+  //   } else if (counter < 20) {
+  //     characterCounter.style.color = "orange"
+  //   } else {
+  //     characterCounter.style.color = "black"
+  //   }
+  // }
+
+  // textArea.addEventListener("input", countCharacters)
+
   return (
     <div>
       {/* <NavBar /> */}
-      <h1 className="create">Create a post</h1>
-      <div className="title">
-        <h3 className="namet">Title</h3>
-        <textarea
-          name="inputTitle"
-          type="text"
-          placeholder="Maximum 100 characters"
-          rows="1"
-          cols="50"
-        ></textarea>
-      </div>
-      <div className="cover">
-        <h3 className="namec">Cover photo</h3>
-        <h3 className="op">(Optional)</h3>
-        <button type="button">Add image</button>
-      </div>
-      <br></br>
-      <div className="body">
-        <h3 className="content">Content</h3>
-        <textarea
-          name="inputContent"
-          type="text"
-          rows="20"
-          cols="100"
-        ></textarea>
-        <button type="button">Add image</button>
-      </div>
-      <br></br>
-      <div className="bottom">
-        <h3 className="topic">Topic</h3>
+      <div className="all">
+        <div className="createTitle">
+          <h1 className="create">Create a post</h1>
+        </div>
+        <br></br>
+        <div className="allcontent">
+          <div className="head">
+            <div className="title">
+              <p className="namet">Title</p>
+              <textarea
+                className="inputTitle"
+                id="inputT"
+                type="text"
+                placeholder="Maximum 70 characters"
+                rows="1"
+                cols="50"
+                maxLength="70"
+              ></textarea>
+              {/* <span id="char_count">70/70</span> */}
+            </div>
+            <div className="allcover">
+              <div className="cover">
+                <p className="namec">Cover photo</p>
+                <p className="op">(Optional)</p>
+              </div>
+              <button type="button" className="btcover">
+                Add image
+              </button>
+            </div>
+          </div>
+          <br></br>
+          <div className="conbody">
+            <p className="content">Content</p>
+            <textarea
+              className="inputContent"
+              id="inputContent"
+              type="text"
+              rows="20"
+              cols="100"
+            ></textarea>
+            <button type="button" className="btcontent">
+              Add image
+            </button>
+          </div>
+          <br></br>
+          <div className="bottom">
+            <p className="topic">Topic</p>
+          </div>
+          <br></br>
+          <div className="btnbottom">
+            <div className="bth">
+              <a href="/home" className="backtohome">
+                BACK TO HOME
+              </a>
+            </div>
+            <button type="button" className="postbtn">
+              POST
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
