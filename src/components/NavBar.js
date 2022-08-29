b4fa12e998359757e3cdfbf8d6202ed683bcae10
prch-s <paranchai.wt@gmail.com>
2022-08-29 11:35:50 +0700

    add NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar.css'
+import {FaRegBell} from 'react-icons/fa'
+
+function NavBar() {
+  return (
+    <div className='Container'>
+        <div className='Kulony'>KULONY</div>
+        <div className='Home'>HOME</div>
+        <div className='MyPost'>MY POST</div>
+        <div className='RequestTopic'>REQUEST TOPIC</div>
+        <div className='CreatePost'>Create new post</div>
+        <FaRegBell className='Bell' size={20}/>
+        
+    </div>
+  )
+}
+
+export default NavBar
