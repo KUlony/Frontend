b478d401d1a3ccc733c68ec1900d157209e2dea0
prch-s <paranchai.wt@gmail.com>
2022-09-03 22:29:56 +0700

    Login

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Login.css";
+import Talk from "./picture/Talk.png"
 
 function Login() {
   return (
-    <div>
-        Login
+    <div className="Login_page">
+
+        <div className="left">
+            <img src={Talk} alt="People talking" className="talk_img"></img>
+        </div>
+
+        <div className="right">
+
+            <h1>LOG-IN</h1>
+            
+            <div className="form-group">
+                <input id="email" type="email" placeholder="EMAIL"></input>
+            </div>
+
+            <div className="form-group">
+                <input id="password" type="password" placeholder="PASSWORD"></input>
+            </div>
+            
+            <button className="login-button" onClick="">LOG IN</button>   
+
+            <div className="sign-up">
+                Don’t have an account?<a href="#">Sign-up</a>
+            </div>
+        </div>
+
     </div>
   );
 }
