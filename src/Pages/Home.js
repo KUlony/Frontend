91a4a411eb338f2b977bf0d655668e848eb6eed5
maikan3002 <kankasem3002@gmail.com>
2022-09-03 20:53:49 +0700

    post generator title photourl like content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,18 +4,39 @@ import Post from "../components/Post";
 import "./Home.css";
 import Think from "../picture/think.png";
 import search from "../picture/search.png";
+import Post_generator from "../components/Post_generator";
 
 function Home() {
+  const testdata = [
+    {
+      post_title: "สวัสดีครับ1",
+      cover_photo_url: "url",
+      post_content:
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum congue ipsum enim, quis sagittis diam dapibus vitae. Nam convallis mollis consectetur. Donec dictum lobortis nibh, et fermentum nisl laoreet ut. Sed metus lectus, viverra gravida mattis sit amet, fringilla elementum lectus. Quisque et pharetra ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non turpis ac mauris commodo scelerisque. Maecenas ut sem sed diam placerat facilisis. Phasellus aliquet vehicula tortor pretium viverra. Morbi tincidunt imperdiet purus, a lobortis nisi auctor eget. Sed pulvinar sollicitudin erat feugiat dictum. Cras purus nisi, sagittis non accumsan id, volutpat nec ipsum. Nunc blandit ante ac velit dictum pretium. Integer neque dui, mattis eu diam sit amet, scelerisque dignissim lacus.",
+      like: 20,
+    },
+    {
+      post_title: "สวัสดีครับ2",
+      cover_photo_url: "url",
+      post_content:
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum congue ipsum enim, quis sagittis diam dapibus vitae. Nam convallis mollis consectetur. Donec dictum lobortis nibh, et fermentum nisl laoreet ut. Sed metus lectus, viverra gravida mattis sit amet, fringilla elementum lectus. Quisque et pharetra ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non turpis ac mauris commodo scelerisque. Maecenas ut sem sed diam placerat facilisis. Phasellus aliquet vehicula tortor pretium viverra. Morbi tincidunt imperdiet purus, a lobortis nisi auctor eget. Sed pulvinar sollicitudin erat feugiat dictum. Cras purus nisi, sagittis non accumsan id, volutpat nec ipsum. Nunc blandit ante ac velit dictum pretium. Integer neque dui, mattis eu diam sit amet, scelerisque dignissim lacus.",
+      like: 20,
+    },
+    {
+      post_title: "สวัสดีครั3",
+      cover_photo_url: "url",
+      post_content:
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum congue ipsum enim, quis sagittis diam dapibus vitae. Nam convallis mollis consectetur. Donec dictum lobortis nibh, et fermentum nisl laoreet ut. Sed metus lectus, viverra gravida mattis sit amet, fringilla elementum lectus. Quisque et pharetra ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non turpis ac mauris commodo scelerisque. Maecenas ut sem sed diam placerat facilisis. Phasellus aliquet vehicula tortor pretium viverra. Morbi tincidunt imperdiet purus, a lobortis nisi auctor eget. Sed pulvinar sollicitudin erat feugiat dictum. Cras purus nisi, sagittis non accumsan id, volutpat nec ipsum. Nunc blandit ante ac velit dictum pretium. Integer neque dui, mattis eu diam sit amet, scelerisque dignissim lacus.",
+      like: 20,
+    },
+  ];
   return (
     <div className="Home_page">
       <div className="Nav_home">
         <NavBar />
       </div>
       <div className="home_search">
-        <div className="home_search_lorem">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          samjdnmlaskmdlksadl kaslk dmaslkdmlkasm
-        </div>
+        <div className="home_search_lorem">Kulony Best Community</div>
         <img src={Think} alt="Girl in a jacket" className="think_img"></img>
         <div className="home_search_input">
           <form>
@@ -37,14 +58,7 @@ function Home() {
           <div className="Topic">Discover</div>
           <div className="Topic">General</div>
         </nav>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post_generator data={testdata} />
       </div>
     </div>
   );
---
+++ src/components/Post_generator.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Post from "./Post";
+
+function Post_generator(props) {
+  const { data } = props;
+  return (
+    <div>
+      {data.map((element) => {
+        return (
+          <Post
+            title={element.post_title}
+            like={element.like}
+            post_content={element.post_content}
+            photo={element.cover_photo_url}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Post_generator;
