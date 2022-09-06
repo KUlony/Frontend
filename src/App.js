8269320c13c378d8cce0d920496126dece0e4cdb
maikan3002 <kankasem3002@gmail.com>
2022-09-06 11:52:01 +0700

    update_app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import "./App.css"
+import "./App.css";
 // import NavBar from "./components/NavBar";
-<<<<<<< HEAD
-import Home from "./Pages/Home"
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import MyPost from "./Pages/MyPost"
-import Profile from "./Pages/Profile"
-=======
+
 import Home from "./Pages/Home";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MyPost from "./Pages/MyPost";
 import Profile from "./Pages/Profile";
 import View_post from "./Pages/View_post";
->>>>>>> 02b951e0a52677933990a424a26bc527b343de6b
 
 function App() {
   return (
@@ -28,7 +22,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
