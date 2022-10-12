eacc03ec0c644ebf2e402d72c6a1b19a93cba610
Jib's macbook <jib@Jib.local>
2022-10-12 12:34:56 +0700

    fix-month-year-edu

--- src/Pages/AddEducation.js
+++ src/Pages/AddEducation.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './AddEducation.css'
+import { dateMonth, dateYear } from './data/monthYear'
 const AddEducation = (props) => {
   const { onBgClick } = props
-  console.log('hello im in add education')
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
   return (
     <div className="add-education">
       <div className="add-education-bg" onClick={onBgClick}></div>
@@ -71,94 +74,24 @@ const AddEducation = (props) => {
             <span>Start date </span>
             <span className="optional">(Optional)</span>
           </div>
-          <select value="start-date-month" id="start-month">
-            <option value="">Month</option>
-            <option value="01">January</option>
-            <option value="02">February</option>
-            <option value="03">March</option>
-            <option value="04">April</option>
-            <option value="05">May</option>
-            <option value="06">June</option>
-            <option value="07">July</option>
-            <option value="08">August</option>
-            <option value="09">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
+          <select
+            onChange={handleChange}
+            name="start-date-month"
+            value="start-date-month"
+            id="start-month"
+          >
+            {dateMonth.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.text}
+              </option>
+            ))}
           </select>{' '}
           <select value="start-date-year" id="start-year">
-            <option value="">Year</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-            <option value="2012">2012</option>
-            <option value="2011">2011</option>
-            <option value="2010">2010</option>
-            <option value="2009">2009</option>
-            <option value="2008">2008</option>
-            <option value="2007">2007</option>
-            <option value="2006">2006</option>
-            <option value="2005">2005</option>
-            <option value="2004">2004</option>
-            <option value="2003">2003</option>
-            <option value="2002">2002</option>
-            <option value="2001">2001</option>
-            <option value="2000">2000</option>
-            <option value="1999">1999</option>
-            <option value="1998">1998</option>
-            <option value="1997">1997</option>
-            <option value="1996">1996</option>
-            <option value="1995">1995</option>
-            <option value="1994">1994</option>
-            <option value="1993">1993</option>
-            <option value="1992">1992</option>
-            <option value="1991">1991</option>
-            <option value="1990">1990</option>
-            <option value="1989">1989</option>
-            <option value="1988">1988</option>
-            <option value="1987">1987</option>
-            <option value="1986">1986</option>
-            <option value="1985">1985</option>
-            <option value="1984">1984</option>
-            <option value="1983">1983</option>
-            <option value="1982">1982</option>
-            <option value="1981">1981</option>
-            <option value="1980">1980</option>
-            <option value="1979">1979</option>
-            <option value="1978">1978</option>
-            <option value="1977">1977</option>
-            <option value="1976">1976</option>
-            <option value="1975">1975</option>
-            <option value="1974">1974</option>
-            <option value="1973">1973</option>
-            <option value="1972">1972</option>
-            <option value="1971">1971</option>
-            <option value="1970">1970</option>
-            <option value="1969">1969</option>
-            <option value="1968">1968</option>
-            <option value="1967">1967</option>
-            <option value="1966">1966</option>
-            <option value="1965">1965</option>
-            <option value="1964">1964</option>
-            <option value="1963">1963</option>
-            <option value="1962">1962</option>
-            <option value="1961">1961</option>
-            <option value="1960">1960</option>
-            <option value="1959">1959</option>
-            <option value="1958">1958</option>
-            <option value="1957">1957</option>
-            <option value="1956">1956</option>
-            <option value="1955">1955</option>
-            <option value="1954">1954</option>
-            <option value="1953">1953</option>
-            <option value="1952">1952</option>
+            {dateYear.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.text}
+              </option>
+            ))}
           </select>
         </section>
         <section className="end-date">
@@ -167,93 +100,18 @@ const AddEducation = (props) => {
             <span className="optional">(Optional)</span>
           </div>
           <select value="end-date-month" name="end-month">
-            <option value="">Month</option>
-            <option value="01">January</option>
-            <option value="02">February</option>
-            <option value="03">March</option>
-            <option value="04">April</option>
-            <option value="05">May</option>
-            <option value="06">June</option>
-            <option value="07">July</option>
-            <option value="08">August</option>
-            <option value="09">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
+            {dateMonth.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.text}
+              </option>
+            ))}
           </select>{' '}
           <select value="end-date-year" name="end-year">
-            <option value="">Year</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-            <option value="2012">2012</option>
-            <option value="2011">2011</option>
-            <option value="2010">2010</option>
-            <option value="2009">2009</option>
-            <option value="2008">2008</option>
-            <option value="2007">2007</option>
-            <option value="2006">2006</option>
-            <option value="2005">2005</option>
-            <option value="2004">2004</option>
-            <option value="2003">2003</option>
-            <option value="2002">2002</option>
-            <option value="2001">2001</option>
-            <option value="2000">2000</option>
-            <option value="1999">1999</option>
-            <option value="1998">1998</option>
-            <option value="1997">1997</option>
-            <option value="1996">1996</option>
-            <option value="1995">1995</option>
-            <option value="1994">1994</option>
-            <option value="1993">1993</option>
-            <option value="1992">1992</option>
-            <option value="1991">1991</option>
-            <option value="1990">1990</option>
-            <option value="1989">1989</option>
-            <option value="1988">1988</option>
-            <option value="1987">1987</option>
-            <option value="1986">1986</option>
-            <option value="1985">1985</option>
-            <option value="1984">1984</option>
-            <option value="1983">1983</option>
-            <option value="1982">1982</option>
-            <option value="1981">1981</option>
-            <option value="1980">1980</option>
-            <option value="1979">1979</option>
-            <option value="1978">1978</option>
-            <option value="1977">1977</option>
-            <option value="1976">1976</option>
-            <option value="1975">1975</option>
-            <option value="1974">1974</option>
-            <option value="1973">1973</option>
-            <option value="1972">1972</option>
-            <option value="1971">1971</option>
-            <option value="1970">1970</option>
-            <option value="1969">1969</option>
-            <option value="1968">1968</option>
-            <option value="1967">1967</option>
-            <option value="1966">1966</option>
-            <option value="1965">1965</option>
-            <option value="1964">1964</option>
-            <option value="1963">1963</option>
-            <option value="1962">1962</option>
-            <option value="1961">1961</option>
-            <option value="1960">1960</option>
-            <option value="1959">1959</option>
-            <option value="1958">1958</option>
-            <option value="1957">1957</option>
-            <option value="1956">1956</option>
-            <option value="1955">1955</option>
-            <option value="1954">1954</option>
-            <option value="1953">1953</option>
-            <option value="1952">1952</option>
+            {dateYear.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.text}
+              </option>
+            ))}
           </select>
         </section>
         <button>Save</button>
