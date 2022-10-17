658a982be6f9cf85f5b42c7091aa3b0a6bf1211c
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-17 12:08:17 +0700

    add-edu css

--- src/Pages/AddEducation.js
+++ src/Pages/AddEducation.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import './AddEducation.css'
-import { dateMonth, dateYear } from './data/monthYear'
+import React from 'react';
+import './AddEducation.css';
+import { dateMonth, dateYear } from './data/monthYear';
+import { FiTrash } from 'react-icons/fi';
 const AddEducation = (props) => {
-  const { onBgClick } = props
+  const { onBgClick } = props;
   const handleChange = (event) => {
-    console.log(event.target.value)
-  }
+    console.log(event.target.value);
+  };
   return (
     <div className="add-education">
       <div className="add-education-bg" onClick={onBgClick}></div>
       <main className="add-education-inner">
-        <header>
+        <header className="add-edu-header">
           Add Education
           <button
             onClick={onBgClick}
@@ -21,6 +22,7 @@ const AddEducation = (props) => {
               background: 'none',
             }}
           >
+            {/* <img src={require('../picture/pencil.png')} alt='pencil' className='pencil-pic'/> */}
             <img
               src={require('../picture/close.png')}
               alt="close"
@@ -29,48 +31,48 @@ const AddEducation = (props) => {
                 height: '16px',
                 cursor: 'pointer',
               }}
+              className="close-add-edu"
             />
           </button>
         </header>
         <section className="school">
-          {' '}
-          School
-          <br />
-          <textarea
+          <div className="school-text">School</div>
+          <input
             class="input-school"
             type="text"
             placeholder="Kasetsart University"
             rows="1"
             cols="20"
-          ></textarea>
+          ></input>
         </section>
         <section className="degree">
-          <span>Degree </span>
-          <span className="optional">(Optional)</span>
-          <br />
-          <textarea
+          <div className="degree-text">
+            <span>Degree </span>
+            <span className="optional">(Optional)</span>
+          </div>
+          <input
             class="input-degree"
             type="text"
             placeholder="Bachelor's degree"
             rows="1"
             cols="20"
-          ></textarea>
+          ></input>
         </section>
         <section className="field-of-study">
-          <div className="field-box">
+          <div className="fos-text">
             <span>Field of study </span>
             <span className="optional">(Optional)</span>
           </div>
-          <textarea
+          <input
             class="input-field"
             type="text"
             placeholder="Computer engineering"
             rows="1"
             cols="20"
-          ></textarea>
+          ></input>
         </section>
         <section className="start-date">
-          <div className="start-box">
+          <div className="start-date-text">
             <span>Start date </span>
             <span className="optional">(Optional)</span>
           </div>
@@ -79,6 +81,7 @@ const AddEducation = (props) => {
             name="start-date-month"
             value="start-date-month"
             id="start-month"
+            className="start-month"
           >
             {dateMonth.map((option, index) => (
               <option key={index} value={option.value}>
@@ -86,7 +89,11 @@ const AddEducation = (props) => {
               </option>
             ))}
           </select>{' '}
-          <select value="start-date-year" id="start-year">
+          <select
+            value="start-date-year"
+            id="start-year"
+            className="start-year"
+          >
             {dateYear.map((option, index) => (
               <option key={index} value={option.value}>
                 {option.text}
@@ -95,18 +102,18 @@ const AddEducation = (props) => {
           </select>
         </section>
         <section className="end-date">
-          <div className="end-box">
-            <span>End date </span>
+          <div className="end-date-text">
+            <span>End date / Expected end date </span>
             <span className="optional">(Optional)</span>
           </div>
-          <select value="end-date-month" name="end-month">
+          <select value="end-date-month" name="end-month" className="end-month">
             {dateMonth.map((option, index) => (
               <option key={index} value={option.value}>
                 {option.text}
               </option>
             ))}
           </select>{' '}
-          <select value="end-date-year" name="end-year">
+          <select value="end-date-year" name="end-year" className="end-year">
             {dateYear.map((option, index) => (
               <option key={index} value={option.value}>
                 {option.text}
@@ -114,11 +121,18 @@ const AddEducation = (props) => {
             ))}
           </select>
         </section>
-        <button>Save</button>
+        <div className="footer-box">
+          <div className="add-edu-delete-edu">
+          <div className="add-edu-delete-text">
+            Delete Education 
+          </div><FiTrash size={18}></FiTrash>
+          </div>
+          <button className="add-edu-save-button">SAVE</button>
+        </div>
       </main>
       {/* <div className="cover_page_education" onClick={onBgClick}></div> */}
     </div>
-  )
-}
+  );
+};
 
-export default AddEducation
+export default AddEducation;
