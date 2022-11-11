d4db490631301ac45eeb8edd489a7a83a9b4064b
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-11-11 13:32:34 +0700

    Done request topic admin

--- src/Pages/ReqtopicAdmin.js
+++ src/Pages/ReqtopicAdmin.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react"
 import Navbar from "../components/NavBar"
 import "./ReqtopicAdmin.css"
 import reqtopicpic from "../picture/0710 1.png"
-import picuser from "../picture/Image.png"
 import axios from "axios"
 import Addtopic_admin from "../components/Addtopic_admin"
 
 function ReqtopicAdmin() {
-  const [postdata, setPostdata] = useState([])
+  const [postdata, setPostdata] = useState(null)
   const [cataname, setcataname] = useState([])
-  const [nametopic, setNametopic] = useState([])
+  const [nametopic, setNametopic] = useState(null)
   const [edittopicheck, seteditTopicCheck] = useState(true)
 
   const topicselect = () => {
@@ -44,20 +43,19 @@ function ReqtopicAdmin() {
   }, [])
 
   const deletetopic = (e) => {
-    // axios
-    //   .delete(`/api/admin/remove_request_topic/${e.request_id}`, {
-    //     headers: {
-    //       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcmFtZWVub25AZ21haWwuY29tIiwiaWQiOiI2MzQ1NzY3ZjJiOTVlZTlmOWMwYTY2M2QiLCJ2ZXJpZmllZCI6dHJ1ZSwiaWF0IjoxNjY4MDU4NzY3LCJleHAiOjE2NjgxNDUxNjd9.NJQU4HZ6PGXYigF-G3P5B0-zieqjl4y4jWq4qUMovG8`,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     const data = res.data
-    //     console.log(data)
-    //   })
-    //   .catch((err) => {
-    //     console.log(err)
-    //   })
-    console.log(e)
+    axios
+      .delete(`/api/admin/remove_request_topic/${e.request_id}`, {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcmFtZWVub25AZ21haWwuY29tIiwiaWQiOiI2MzQ1NzY3ZjJiOTVlZTlmOWMwYTY2M2QiLCJ2ZXJpZmllZCI6dHJ1ZSwiaWF0IjoxNjY4MTAyMTM0LCJleHAiOjE2NjgxODg1MzR9.oIbRkgrR4b7tSaEySHYyVig26NBFTdSYdsLBteNdfKg`,
+        },
+      })
+      .then((res) => {
+        const data = res.data
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
@@ -68,40 +66,56 @@ function ReqtopicAdmin() {
           <div className="namepagereq">Requested Topics</div>
           <img alt="" src={reqtopicpic} className="topicpic"></img>
         </div>
-        <div className="bodyreq">
-          {postdata.map((item, index) => (
-            <div className="topiccard" key={index} id={item.request_id}>
-              <div className="headcardreq">
-                <img alt="" src={picuser} className="picuser"></img>
-                <p className="nametopicreq">{item.request_topic}</p>
+        {postdata && (
+          <div className="bodyreq">
+            {postdata.map((item, index) => (
+              <div className="topiccard" key={index} id={item.request_id}>
+                <div className="headcardreq">
+                  <img
+                    alt=""
+                    src={item.profile_pic_url}
+                    className="picuser"
+                  ></img>
+                  <p className="nametopicreq">{item.request_topic}</p>
+                </div>
+                <div className="buttonreqtopic">
+                  <p className="datecardreq">
+                    {item.year}-{item.month}-{item.day}
+                  </p>
+                  <button
+                    className="btnaddtopic"
+                    onClick={() => {
+                      topicselect()
+                      setNametopic(item)
+                    }}
+                  >
+                    Add Topic <i class="bi bi-check-lg"></i>
+                  </button>
+                  <h2
+                    className="trashreq"
+                    onClick={() => {
+                      deletetopic(item)
+                      window.location.reload()
+                    }}
+                  >
+                    <i class="bi bi-trash"></i>
+                  </h2>
+                </div>
               </div>
-              <div className="buttonreqtopic">
-                <p className="datecardreq">{item.requset_time}</p>
-                <button
-                  className="btnaddtopic"
-                  onClick={() => {
-                    topicselect()
-                    setNametopic(item)
-                  }}
-                >
-                  Add Topic <i class="bi bi-check-lg"></i>
-                </button>
-                <h2 className="trashreq">
-                  <i class="bi bi-trash"></i>
-                </h2>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
-      <div className={`addtopiccss ${edittopicheck ? "nothing" : ""}`}>
-        <Addtopic_admin cata={cataname} datatopic={nametopic} />
-        <div className="cancleconfirm">
-          <p className="cancleaddtopic" onClick={topicselect}>
-            CANCEL
-          </p>
+      {nametopic && (
+        <div className={`addtopiccss ${edittopicheck ? "nothing" : ""}`}>
+          <Addtopic_admin cata={cataname} datatopic={nametopic} />
+          <div className="cancleconfirm">
+            <p className="cancleaddtopic" onClick={topicselect}>
+              CANCEL
+            </p>
+          </div>
         </div>
-      </div>
+      )}
       {!edittopicheck && <div className="displayback"></div>}
     </div>
   )
--- src/components/Addtopic_admin.js
+++ src/components/Addtopic_admin.js
@@ -3,13 +3,59 @@ import React, { useEffect, useState } from "react"
 import "./Addtopic_admin.css"
 
 const Addtopic_admin = ({ cata, datatopic }) => {
-  // const {postcata} = props
   const [editname, setEditname] = useState(true)
+  const [senddatabtn, setSenddatabtn] = useState(true)
+  const [sendtopic, setSendtopic] = useState("")
+
+  // useEffect(() => {
+  // setTimeout(() => {
+  // setTopicname(datatopic.request_topic)
+  //   if (state) {
+  //   }
+  // setState(false)
+  // }, 100)
+  // }, [])
 
   const token = localStorage.getItem("item")
 
   const editnamehandle = () => {
     setEditname(!editname)
+  }
+
+  const handlecata = (e) => {
+    setSendtopic(e.target.value)
+    if (e.target.value === "none") {
+      setSenddatabtn(true)
+    } else {
+      setSenddatabtn(false)
+    }
+    console.log(e.target.value)
+  }
+
+  const addtopic = (e) => {
+    const nametopic = document.getElementById("topicname")
+    console.log(nametopic.value)
+    console.log(sendtopic)
+    axios
+      .post(
+        `https://kulony-backend.herokuapp.com/api/admin/accept_request_topic/${datatopic.request_id}`,
+        {
+          catagory_id: sendtopic,
+          topic_name: nametopic.value,
+        },
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcmFtZWVub25AZ21haWwuY29tIiwiaWQiOiI2MzQ1NzY3ZjJiOTVlZTlmOWMwYTY2M2QiLCJ2ZXJpZmllZCI6dHJ1ZSwiaWF0IjoxNjY4MTAyMTM0LCJleHAiOjE2NjgxODg1MzR9.oIbRkgrR4b7tSaEySHYyVig26NBFTdSYdsLBteNdfKg`,
+          },
+        }
+      )
+      .then((res) => {
+        const data = res.data
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
@@ -21,11 +67,10 @@ const Addtopic_admin = ({ cata, datatopic }) => {
           <p className="topicnameedit">Topic : </p>
           <textarea
             className={`${editname ? "topicnametext" : "outlinebox"}`}
-            placeholder="Topic name"
-            // maxLength="35"
+            placeholder={datatopic.request_topic}
+            maxLength="50"
             id="topicname"
             disabled={editname}
-            value={datatopic.request_topic}
           ></textarea>
           <h3 className="icontopic" onClick={editnamehandle}>
             <i class="bi bi-slash-circle-fill"></i>
@@ -34,15 +79,15 @@ const Addtopic_admin = ({ cata, datatopic }) => {
         <div className="addtocata">
           <p className="catanameedit">Add this topic to : </p>
           <div className="dropboxcata">
-            <select className="cataname" placeholder="Categories">
+            <select
+              className="cataname"
+              placeholder="Categories"
+              onChange={(e) => handlecata(e)}
+            >
+              <option value="none">-Categories-</option>
               {cata.map((item, index) => (
-                <option
-                  key={index}
-                  value={item.catagory_name}
-                  id={item.catagory_id}
-                >
+                <option key={index} value={item.catagory_id}>
                   {item.catagory_name}
-                  {/* {console.log(item)} */}
                 </option>
               ))}
             </select>
@@ -50,7 +95,16 @@ const Addtopic_admin = ({ cata, datatopic }) => {
         </div>
       </div>
       <div className="bottomaddtopic">
-        <button type="button" className="confirmaddtopic" id="confirmaddtopic">
+        <button
+          type="button"
+          className={`${senddatabtn ? "dis" : "confirmaddtopic"}`}
+          id="confirmaddtopic"
+          onClick={() => {
+            addtopic()
+            window.location.reload()
+          }}
+          disabled={senddatabtn}
+        >
           CONFIRM
         </button>
       </div>
